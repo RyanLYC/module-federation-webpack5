38d21480da56f9cc2343b16395ff33973a54865e
Ryan <656040874@qq.com>
2023-07-13 09:52:38 +0800

    -webpack5-module-federation

---
+++ vue-webpack5-module-federation/app2/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <Header name="app2 - 666" />
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    Header: () => import("app1/Header"),
+  },
+};
+</script>
